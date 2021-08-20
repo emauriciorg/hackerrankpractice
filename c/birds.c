a1e1456925b738639a04a190003dff2e7be60818
Mauro <emauriciorg@gmail.com>
2021-08-20 01:31:59 -0500

    feat (birds) : added bird frequency counter with merge sort and multiple birds

--- c/birds.c
+++ c/birds.c
@@ -4,6 +4,77 @@
 #include <string.h>
 
 
+/* C program for Merge Sort */
+#include <stdio.h>
+#include <stdlib.h>
+ 
+// Merges two subarrays of arr[].
+// First subarray is arr[l..m]
+// Second subarray is arr[m+1..r]
+void merge(int arr[], int l, int m, int r)
+{
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+ 
+    /* create temp arrays */
+    int L[n1], R[n2];
+ 
+    /* Copy data to temp arrays L[] and R[] */
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+ 
+    /* Merge the temp arrays back into arr[l..r]*/
+    i = 0; // Initial index of first subarray
+    j = 0; // Initial index of second subarray
+    k = l; // Initial index of merged subarray
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+ 
+    /* Copy the remaining elements of L[], if there
+    are any */
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+ 
+    /* Copy the remaining elements of R[], if there
+    are any */
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+ 
+/* l is for left index and r is right index of the
+sub-array of arr to be sorted */
+void mergeSort(int arr[], int l, int r)
+{
+    if (l < r) {
+        // Same as (l+r)/2, but avoids overflow for
+        // large l and h
+        int m = l + (r - l) / 2;
+ 
+        // Sort first and second halves
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+ 
+        merge(arr, l, m, r);
+    }
+}
 
 void sort_numbers(int n, int *arr){
      int aux=0;
@@ -18,45 +89,111 @@ void sort_numbers(int n, int *arr){
      }
  }
 
+void print_array(int n, int *arr){
+    printf("Array is \n");
+    for (int i= 0; i< n;i++){
+        printf(" %d ", *(arr+i));
 
+    }
+    printf("\n");
+}
+
+struct bird_stat_st{
+    int type;
+    int counter;
+};
+
+struct bird_stat_st bpresent={0,0};
+struct bird_stat_st bold={0,0};
 
 int migratoryBirds(int arr_count, int* arr) {
-    
+        print_array(arr_count,arr);
+      mergeSort(arr, 0, arr_count - 1);
+//        sort_numbers(arr_count,arr);
+         print_array(arr_count,arr);
 
+        bpresent.type = *(arr);
+        bold.type = *(arr);
+        bpresent.counter = 1;
+        bold.counter = 1;
 
-        sort_numbers(arr_count,arr);
-        int frequency_new = 0;
-        int frequency_old = 0;
-        int current_bird_type = *(arr);
-        int next_bird_type = *(arr);
+        
         int most_repeated = 0;
 
-        for(int i = 0; i < arr_count; i++){
+        for(int i = 1; i < arr_count; i++){
 
-            if (current_bird_type == *(arr+i)){
-                   frequency_new++;
-                    printf("increasing [%d][%d]\n",current_bird_type,frequency_new);
+                 printf("\n");
+                 printf("Comp[n==o] [%d ==%d]  new [cn ==co]  [%d=%d] o =%d", 
+                        bpresent.type,
+                        *(arr+i),
+                        bpresent.counter,
+                        bold.counter,
+                        bold.type);
+            if ( bpresent.type == *(arr+i)){
+                    bpresent.counter++;
+                    
+                    printf("++");
 
             }else{
 
-                if ( frequency_old != 0  && (frequency_old > frequency_new) ){
+                // if (bold.counter == 1){
+                    
+                //     bold.counter = bpresent.counter;
+                //     bold.type    =  bpresent.type;
+
+                //     bpresent.counter=1;
+                //     bpresent.type = *(arr+i);
+                //     continue;
+                // }
+
+                if ( bold.counter > bpresent.counter ){
+                        bpresent.counter=1;
+                        bpresent.type = *(arr+i);
+                        continue;
                 }
-                else{
-                    frequency_old = frequency_new;
-                    next_bird_type = current_bird_type;
+                if ( bold.counter < bpresent.counter ){
+                        bold.counter = bpresent.counter;
+                        bold.type =  bpresent.type;
+                        bpresent.counter=1;
+                        bpresent.type = *(arr+i);
+                        continue;
                 }
 
-                   frequency_new=0;
-                   current_bird_type = *(arr+i);
+                if(bold.counter == bpresent.counter)
+                {
+                    if(bpresent.type < bold.type){
+                    
+                        bold.counter = bpresent.counter;
+                        bold.type =  bpresent.type;
+                         // continue;
+                    }else{
+                          // bold.counter = bold.counter;
+                           // bold.type =  bpresent.type;
+                    }
+                     bpresent.counter=1;
+                        bpresent.type = *(arr+i);
+                }
+
+                 
             }
         }
-        if(frequency_old == frequency_new){
-            most_repeated = current_bird_type;
-            if(next_bird_type <current_bird_type ){
-                most_repeated = next_bird_type;
+        if( bold.type == bpresent.type){
+            most_repeated =  bpresent.type;
+            
+            if( bold.type < bpresent.type ){
+                bpresent.counter =  bold.counter;
+                 most_repeated   =  bold.type;
+            }
+        }else{
+            if (bold.counter > bpresent.counter){
+                bpresent.counter = bold.counter;
+                bpresent.type    = bold.type;
+                most_repeated    = bpresent.type; 
+            }else{
+                most_repeated = bpresent.type;
             }
         }
-        printf("found bigger id[%d] f[%d]\n", most_repeated,frequency_new );
+        printf("\n found bigger id[%d] f[%d]\n", most_repeated,bpresent.counter );
 
         return most_repeated;
 }
@@ -64,8 +201,8 @@ int migratoryBirds(int arr_count, int* arr) {
 
 void main(void)
 {
-    int n= 11;
-    int birds[12] = {1, 2, 3 , 4 , 5 , 4 , 3 , 2 , 1 , 3, 4};
+    int n= 13;
+    int birds[13] = {1,4,4,4,5,3,5,8,56,56,56,56,56};
     int frequency = migratoryBirds(n, birds);
     
     printf("most repeated [%d]\n", frequency);
