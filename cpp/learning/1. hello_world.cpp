184d48ff68be1d6a178e709d8d2bd29199f1aed0
Mauro <emauriciorg@gmail.com>
2021-08-28 00:30:39 -0500

    feat(cpp) : added simple function using the std namespace

--- cpp/learning/1. hello_world.cpp
+++ cpp/learning/1. hello_world.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 
+using namespace std;
 
+
+double square(double x)
+{
+    return x*x;
+}
+
+void print_square(double x)
+{
+      cout << "The square of "<<x<<" is "<<square(x)<<"\n";
+
+}
 int main (void){
-
-   std::cout << "Hello World!\n";
+    print_square(1.234);
 }
